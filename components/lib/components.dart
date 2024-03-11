b8f491a834b667d260846fb5d11d97a0ab4d7f96
klukovka <yuliia.koba@proarea.com>
2024-03-11 22:01:35 +0200

    LoadMoreScrollListener

--- components/lib/components.dart
+++ components/lib/components.dart
@@ -1,1 +1,3 @@
 library components;
+
+export 'src/scrollable/load_more_scroll_listener.dart';
--- presentation/lib/src/pages/home_page/locations_tab/locations_tab.dart
+++ presentation/lib/src/pages/home_page/locations_tab/locations_tab.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:components/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:presentation/src/bloc.dart';
+import 'package:presentation/src/controllers.dart';
 import 'package:presentation/src/core/extensions/build_context_extension.dart';
 
 @RoutePage()
@@ -20,9 +22,14 @@ class LocationsTab extends StatelessWidget implements AutoRouteWrapper {
   Widget build(BuildContext context) {
     return BlocBuilder<LocationsTabCubit, LocationsTabState>(
       builder: (context, state) {
-        return ListView.builder(
-          itemBuilder: (context, index) => Text(state.locations[index].name),
-          itemCount: state.locations.length,
+        return LoadMoreScrollListener(
+          loadMore: () => context
+              .locator<LocationsTabController>()
+              .uploadNextPage(state.filter),
+          child: ListView.builder(
+            itemBuilder: (context, index) => Text(state.locations[index].name),
+            itemCount: state.locations.length,
+          ),
         );
       },
     );
