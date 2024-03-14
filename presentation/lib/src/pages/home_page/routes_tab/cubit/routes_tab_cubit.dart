b89119de2e841a49ac021924b7f79bdbcf0063c4
klukovka <yuliia.koba@proarea.com>
2024-03-15 01:34:31 +0200

    RoutesTabCubit

--- presentation/lib/src/pages/home_page/routes_tab/cubit/routes_tab_cubit.dart
+++ presentation/lib/src/pages/home_page/routes_tab/cubit/routes_tab_cubit.dart
@@ -1,8 +1,27 @@
-import 'package:bloc/bloc.dart';
+import 'package:domain/domain.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'routes_tab_state.dart';
 
 class RoutesTabCubit extends Cubit<RoutesTabState> {
-  RoutesTabCubit() : super(RoutesTabCubitInitial());
+  RoutesTabCubit() : super(const RoutesTabState());
+
+  void setFilter(Filter filter) => emit(state.copyWith(
+        filter: filter,
+        isLoading: true,
+      ));
+
+  void setRoutes(List<Route> routes, {int? amount}) => emit(state.copyWith(
+        routes: routes,
+        isLoading: false,
+        totslRoutesAmount: amount,
+      ));
+
+  void appendRoutes(List<Route> routes) => emit(state.copyWith(
+        routes: [...state.routes, ...routes],
+        isLoading: false,
+      ));
+
+  void stopLoading() => emit(state.copyWith(isLoading: false));
 }
--- presentation/lib/src/pages/home_page/routes_tab/cubit/routes_tab_state.dart
+++ presentation/lib/src/pages/home_page/routes_tab/cubit/routes_tab_state.dart
@@ -1,10 +1,34 @@
 part of 'routes_tab_cubit.dart';
 
-sealed class RoutesTabState extends Equatable {
-  const RoutesTabState();
+class RoutesTabState extends Equatable {
+  final Filter filter;
+  final bool isLoading;
+  final int totslRoutesAmount;
+  final List<Route> routes;
+
+  const RoutesTabState({
+    this.filter = const Filter(),
+    this.isLoading = true,
+    this.totslRoutesAmount = 0,
+    this.routes = const [],
+  });
+
+  bool get isFirstLoading => isLoading && routes.isEmpty;
+
+  RoutesTabState copyWith({
+    Filter? filter,
+    bool? isLoading,
+    int? totslRoutesAmount,
+    List<Route>? routes,
+  }) {
+    return RoutesTabState(
+      filter: filter ?? this.filter,
+      isLoading: isLoading ?? this.isLoading,
+      totslRoutesAmount: totslRoutesAmount ?? this.totslRoutesAmount,
+      routes: routes ?? this.routes,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [filter, isLoading, totslRoutesAmount, routes];
 }
-
-final class RoutesTabCubitInitial extends RoutesTabState {}
