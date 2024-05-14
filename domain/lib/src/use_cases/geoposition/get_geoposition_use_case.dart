f9098f6d73e5cce6af4419cf343e66049dae31fb
klukovka <yuliia.koba@proarea.com>
2024-05-15 00:07:32 +0300

    GetGeopositionUseCase

---
+++ domain/lib/src/use_cases/geoposition/get_geoposition_use_case.dart
@@ -0,0 +1,42 @@
+import 'package:domain/domain.dart';
+
+class GetGeopositionUseCase {
+  final GeopositionRepository geopositionRepository;
+  final GeopositionOutputPort geopositionOutputPort;
+  final ErrorHandlerOutputPort errorHandlerOutputPort;
+
+  GetGeopositionUseCase({
+    required this.geopositionRepository,
+    required this.geopositionOutputPort,
+    required this.errorHandlerOutputPort,
+  });
+
+  Future<void> call() async {
+    final serviceEnabled = await geopositionRepository.checkServiceEnabled();
+
+    if (serviceEnabled.hasFailed) {
+      errorHandlerOutputPort.setError(serviceEnabled.failure!);
+      geopositionOutputPort.setGeopositionEnabled(false);
+      return;
+    }
+
+    final permission = await geopositionRepository.checkServiceEnabled();
+    if (serviceEnabled.hasFailed) {
+      errorHandlerOutputPort.setError(permission.failure!);
+      geopositionOutputPort.setGeopositionEnabled(false);
+      return;
+    }
+
+    final stream = geopositionRepository.getCurrentPositionStream();
+
+    if (stream.hasFailed) {
+      errorHandlerOutputPort.setError(stream.failure!);
+      geopositionOutputPort.setGeopositionEnabled(false);
+      return;
+    }
+
+    geopositionOutputPort.setGeopositionEnabled(true);
+
+    stream.result?.listen(geopositionOutputPort.updateUserPosition);
+  }
+}
