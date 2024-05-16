ee23435d26c55ae7551639c550250bdb7f52547d
klukovka <yuliia.koba@proarea.com>
2024-05-16 22:42:34 +0300

    add basic fields

--- presentation/lib/src/pages/routes/create_route_page/create_route_page.dart
+++ presentation/lib/src/pages/routes/create_route_page/create_route_page.dart
@@ -1,5 +1,14 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:components/components.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+enum _CreateRoutePageField {
+  name,
+  description,
+  private,
+  locations,
+}
 
 @RoutePage()
 class CreateRoutePage extends StatefulWidget {
@@ -10,11 +19,54 @@ class CreateRoutePage extends StatefulWidget {
 }
 
 class _CreateRoutePageState extends State<CreateRoutePage> {
+  final _fbKey = GlobalKey<FormBuilderState>();
+
+  FormBuilderState? get _fbState => _fbKey.currentState;
+  Map<String, dynamic> get _fbValues => _fbState?.value ?? {};
+
   @override
   Widget build(BuildContext context) {
+    //TODO: Add localization
     return Scaffold(
-      //TODO: Add localization
       appBar: AppBar(title: const Text('Create Route')),
+      body: AutovalidateModeNotificationBuilder(
+        builder: (context, autovalidateMode, child) => FormBuilder(
+          key: _fbKey,
+          autovalidateMode: autovalidateMode,
+          child: child!,
+        ),
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          children: [
+            FormBuilderTextField(
+              name: _CreateRoutePageField.name.name,
+              maxLength: 30,
+              validator: (value) {
+                if (value?.isEmpty ?? true) return 'Field is required';
+                return null;
+              },
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            const SizedBox(height: 12),
+            FormBuilderTextField(
+              name: _CreateRoutePageField.description.name,
+              maxLines: 4,
+              maxLength: 120,
+              validator: (value) {
+                if (value?.isEmpty ?? true) return 'Field is required';
+                return null;
+              },
+              decoration: const InputDecoration(labelText: 'Description'),
+            ),
+            const SizedBox(height: 12),
+            FormBuilderCheckbox(
+              name: _CreateRoutePageField.private.name,
+              title: const Text('Private'),
+              decoration: const InputDecoration(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
