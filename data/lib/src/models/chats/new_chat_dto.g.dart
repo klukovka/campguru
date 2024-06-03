2210ed40138ac1b9a779b23e27751e0dd8940fa5
Yuliia Koba <yuliia.koba@proarea.com>
2024-06-03 20:42:11 +0300

    NewChatDto

---
+++ data/lib/src/models/chats/new_chat_dto.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'new_chat_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$NewChatDtoToJson(NewChatDto instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'owner_id': instance.ownerId,
+      'users': instance.users,
+    };
