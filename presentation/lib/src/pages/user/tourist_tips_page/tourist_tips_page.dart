908ea02972de3851f38aa847c95a2441db014f8a
Yuliia Koba <yuliia.koba@proarea.com>
2024-06-09 18:15:53 +0300

    TouristTipTile

--- presentation/lib/src/pages/user/tourist_tips_page/tourist_tips_page.dart
+++ presentation/lib/src/pages/user/tourist_tips_page/tourist_tips_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:localizations/localizations.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:presentation/src/pages/user/tourist_tips_page/tourist_tip.dart';
 import 'package:presentation/src/pages/user/tourist_tips_page/views/tourist_tip_tile.dart';
 
@@ -14,11 +15,33 @@ class TouristTipsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(context.strings.touristTips),
       ),
-      body: ListView.separated(
-        itemBuilder: (context, index) =>
-            TouristTipTile(touristTip: TouristTip.values[index]),
-        separatorBuilder: (context, index) => const SizedBox(height: 12),
-        itemCount: TouristTip.values.length,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 16, left: 28, right: 28),
+            child: Row(
+              children: [
+                Icon(MdiIcons.campfire),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    context.strings.theseTipsHelpYou,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              padding: const EdgeInsets.all(16),
+              itemBuilder: (context, index) =>
+                  TouristTipTile(touristTip: TouristTip.values[index]),
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
+              itemCount: TouristTip.values.length,
+            ),
+          ),
+        ],
       ),
     );
   }
--- presentation/lib/src/pages/user/tourist_tips_page/views/tourist_tip_tile.dart
+++ presentation/lib/src/pages/user/tourist_tips_page/views/tourist_tip_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:presentation/src/pages/user/tourist_tips_page/tourist_tip.dart';
 
 class TouristTipTile extends StatelessWidget {
@@ -11,6 +12,33 @@ class TouristTipTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              children: [
+                Icon(MdiIcons.lightbulb),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    touristTip.getTitle(context),
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(height: 1, thickness: 2),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(touristTip.getDescription(context)),
+          ),
+        ],
+      ),
+    );
   }
 }
